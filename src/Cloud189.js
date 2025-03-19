e0b1a3e4059709e0dd7fee550a1ce784702ef286
gqq8156 <815616859@qq.com>
2025-03-19 17:05:21 +0800

    Update Cloud189.js

--- src/Cloud189.js
+++ src/Cloud189.js
@@ -213,6 +213,6 @@ const main = async () => {
     logger.log("\n\n");
     const events = recording.replay();
     const content = events.map((e) => `${e.data.join("")}`).join("  \n");
-    push("2350-1天翼云盘签到git", content);
+    push("2350-2天翼云盘签到git", content);
   }
 })();
